37fcece07f0c98458e131e564e403d77f680c948
mimos <luqmanhakim.dev@gmail.com>
2025-02-25 10:36:34 +0800

    script for running both files

---
+++ DockerSend/run.sh
@@ -0,0 +1,5 @@
+# Run app2.py in a new terminal window
+gnome-terminal -- bash -c "python3 app2.py; exec bash"
+
+# Run test.py in another new terminal window
+gnome-terminal -- bash -c "python3 test.py; exec bash"
